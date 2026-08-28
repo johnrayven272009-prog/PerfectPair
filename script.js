de50c27143bfe38ce8d42e714190784eb2389375
johnrayven272009-prog <johnrayven272009@gmail.com>
2026-08-28 21:10:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function openLetter() {
     document.getElementById("opening").classList.add("hidden");
     document.getElementById("letter").classList.remove("hidden");
 }
+
+function showEnding() {
+    document.getElementById("letter").classList.add("hidden");
+    document.getElementById("ending").classList.remove("hidden");
+}
